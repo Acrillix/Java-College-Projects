a6fe11e659fd9e44167f5349634d38204628a19f
Acrillix <43619189+Acrillix@users.noreply.github.com>
2019-12-06 16:22:27 -0500

    Create Half.java

---
+++ Half.java
@@ -0,0 +1,47 @@
+//9-24-2019
+//Half.java; A program that outputs specific lines of information
+//Error: All variables should start with lowercase and camelcase
+
+public class Half { 
+//Class name is Half
+   public static void main(String[] args) {
+   double MathVariable = 10;
+   //Declaring a Double variable MathVariable equal to 10
+      System.out.println(MathVariable/2);
+      System.out.print("\n ");
+      //JAVA follows PEMDAS so doing 10/2 = 5
+      //Escape character for new line
+      System.out.println(MathVariable/2/3);
+      System.out.print("\n ");
+      //Again, following PEMDAS Java does 10 / 2 THEN Divides that amout by three
+       //Escape character for new line
+      System.out.println(MathVariable/2/3+3);
+      System.out.print("\n ");
+      //Finally does 10 / 2 = 5; 5 / 3 + 3
+       //Escape character for new line
+   int SecondMathVariable = 20;
+   //Declaring a integer SecondMathVariable of 20
+      System.out.println(SecondMathVariable/2);
+      System.out.print("\n ");
+      //Calling the integer to divide by 2
+       //Escape character for new line
+      System.out.println(SecondMathVariable/2/3);
+      System.out.print("\n ");
+      //Take the integer and divide it by 2 and then 3
+       //Escape character for new line
+   String FirstName = "Brendan";
+   //Declaring the string FirstName equal to my name
+      System.out.println(FirstName);
+      System.out.print("\n ");
+      //Print my name
+       //Escape character for new line
+   boolean ItsFalse = false; 
+   //Boolean is true/false math so setting my variable to false means everytime I call it it will be false
+      System.out.println(ItsFalse);
+      System.out.print("\n ");
+      //Print false
+       //Escape character for new line
+      
+      
+      }
+}      
